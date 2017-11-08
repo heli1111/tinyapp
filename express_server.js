40e47bb89ceebc887505989cf4cb16a2a882cb22
Jessica <hepu24@gmail.com>
2017-11-08 02:37:53 +0000

    get request to redirect short url to long url and added comments

--- express_server.js
+++ express_server.js
@@ -1,11 +1,10 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -23,24 +22,28 @@ function generateRandomString(num) {
 //set view engine
 app.set("view engine", "ejs")
 
-
+// index page
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+// display urls as json object
 app.get("/urls.json",(req,res) =>{
   res.json(urlDatabase);
 });
 
+// renders a page to list urls in a table
 app.get("/urls", (req,res) =>{
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 })
 
+// renders a page to create new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// redners a page to show single short url and its long url
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
@@ -54,14 +57,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newID}`);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+// redirect short url to long url
+app.get("/u/:shortURL", (req,res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT}!`);
 });
-
-
-
-//https://expressjs.com/en/guide/routing.html
